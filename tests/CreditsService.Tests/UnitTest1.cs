598c240c4a4ea862f92fa749329ef96d18e39a8e
Pavel Khorev <khorev05@list.ru>
2026-08-28 17:02:21 +0700

    Initialize backend structure

---
+++ tests/CreditsService.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CreditsService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/UsersService.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace UsersService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
